f2bd6dc853b4b4dab692ff8a68cbe74f0396ef1c
Ravi Kalia <ravkalia@gmail.com>
2018-04-22 18:35:27 -0500

    real world example and annotations

--- main.js
+++ main.js
@@ -9,10 +9,21 @@ const dog = {
 
 dog.talk();
 
+// this is attached to the global variable
 const talkFunction = dog.talk;
-// this is now attached to the global variable
 
+// there exists no property global.sound, hence undefined
 console.log(talkFunction());
 
+// explicitly give the object which will be this when the function is called
 const boundFunction = talkFunction.bind(dog);
 console.log(boundFunction());
+
+
+// real world example with DOM
+const button = document.getElementById('myNiceButton');
+
+button.addEventListener(
+  'click',
+  dog.talk.bind(dog),
+);
