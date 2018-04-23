05e05a34a3a18b1241ca0e9b5b79c77d4d996f89
Ravi Kalia <ravkalia@gmail.com>
2018-04-22 19:01:16 -0500

    different ways of binding to get around this references

--- main.js
+++ main.js
@@ -20,10 +20,49 @@ const boundFunction = talkFunction.bind(dog);
 console.log(boundFunction());
 
 
-// real world example with DOM
-const button = document.getElementById('myNiceButton');
+// real world example with DOM if run in browser
 
-button.addEventListener(
-  'click',
-  dog.talk.bind(dog),
-);
+// const button = document.getElementById('myNiceButton');
+
+// button.addEventListener(
+//   'click',
+//   dog.talk.bind(dog),
+// );
+
+const talk2 = function (sound) {
+  return sound;
+};
+
+
+const talk3 = function () {
+  return this.sound;
+};
+
+[talk2('wooof!!!!'), talk3('woooof!!!')]
+  .map(x => console.log(x));
+
+const boromir = {
+  sound: 'so you want to go to mordor',
+};
+
+const talkBoundToBoromir = talk3.bind(boromir);
+
+talkBoundToBoromir();
+talk3();
+
+
+const boromir2 = {
+  speak: talk3,
+  sound: 'so you want to go to mordor',
+};
+
+boromir2.speak();
+
+const blabber = boromir2.speak;
+blabber();
+
+boromir2.chat = talk3.bind(boromir);
+
+const blabber2 = boromir2.chat;
+
+blabber2();
